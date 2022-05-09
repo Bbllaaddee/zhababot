08747cbc29539ce9f4077a7077568b9d41558ae3
Bbllaaddee <mr.kamorzin@yandex.ru>
2022-05-09 17:18:50 +0300

    first working sample

--- zhababot/zhaba.py
+++
@@ -1,7 +0,0 @@
-import os
-import random
-
-def random_zhaba():
-    files = os.listdir('./zhaba_pics/')
-    choice = random.choice(files)
-    return open(f'./zhaba_pics/{choice}')
--- zhababot/zhababot.py
+++ zhababot/zhababot.py
@@ -1,4 +1,36 @@
 import telebot
-import handlers
+import os
+import random
+from dotenv import load_dotenv
 
-bot = telebot.Telebot("")
+def random_zhaba():
+    files = os.listdir('./ZHABA/')
+    choice = random.choice(files)
+    return open(f'./ZHABA/{choice}', 'rb')
+
+def random_zhabapoezd():
+    files = os.listdir('./ZHABA_POEZD/')
+    choice = random.choice(files)
+    return open(f'./ZHABA_POEZD/{choice}', 'rb')
+
+load_dotenv()
+token = os.environ.get('TOKEN')
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start', 'help'])
+def welcome(message):
+    reply = "Use /ZHABA for random zhaba! \
+            Use /ZHABAPOEZD for random zhapapoezd!"
+    bot.reply_to(message, reply)
+
+@bot.message_handler(commands=['ZHABAPOEZD', 'zhabapoezd', 'Zhabapoezd'])
+def zhabapoezd(message):
+    chat_id = message.chat.id
+    bot.send_photo(chat_id, random_zhabapoezd())
+
+@bot.message_handler(commands=['ZHABA', 'zhaba', 'Zhaba'])
+def zhaba(message):
+    chat_id = message.chat.id
+    bot.send_photo(chat_id, random_zhaba())
+
+bot.infinity_polling()
